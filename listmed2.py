aa597a2b607b4e4577ad93fc4a2ab285c133183c
YOUR NAME <dluzenst@gmail.com>
2014-11-13 00:21:52 +0100

    exo meds update

--- listmed2.py
+++ listmed2.py
@@ -26,34 +26,43 @@ df['Deremb'] = (df[colNames[9:14]].sum(axis=1)>0) * (df[colNames[15]]==0)
 #rembourses en 2013
 df['Remb'] = (df[colNames[9:14]].sum(axis=1)==0) * (df[colNames[15]]>0)
 
-# ## Trouver dosage, forme
-# nomCourt = df['NOM COURT']
-# produit = df['PRODUIT']
-# df['Dosage'] = ''
-# df['Forme'] = ''
 
-def getInfo(x):
-	a = x['NOM COURT']
-	a.replace(x['PRODUIT'],'+++++++++++').strip()
-	print a, x['PRODUIT']
-	return a
+def getInfo(row):
+	"""Create aux column INFO"""
+	row['INFO'] = row['NOM COURT'].replace(row['PRODUIT'],'').strip()
+	return row
 
 def findDosage(x,regex_dosage):
+	"""Gets med dosage from regex match to field INFO"""
 	result = re.findall(regex_dosage,x)
 	if result:
 		dosage = result[0]
 	else:
-		dosage = ['']
+		dosage = ''
 	return dosage
 
-nomCourt = df['NOM COURT']
-produit = df['PRODUIT']
+def getFormePharma(row):
+	"""Gets med's pharma form from INFO\{DOSAGE}"""
+	if(row['DOSAGE']):
+		row['FORME'] = row['INFO'].replace(row['DOSAGE'],'').strip()
+	else:
+		row['FORME'] = row['INFO']
+	return row
 
-df2=df['NOM COURT','PRODUIT'].apply(lambda x : getInfo(x),axis=1 )
+# Create aux column INFO
+df=df.apply(lambda x : getInfo(x),axis=1 )
 
+# Create DOSAGE column
 regex_dosage = re.compile(
-	r'(\d{1,},?\d{0,}\s?\w+\/?\d{1,},?\d{0,}\s?\w+|\d{1,},?\d{0,}\s?\w+\/?\w+|\d{1,},?\d{0,}\%)')
-df['dosage'] = df2['NOM COURT'].apply(lambda x : findDosage(x,regex_dosage) )
+	r'(\d{1,},?\d{0,}\s?\w+\/?\d{1,},?\d{0,}\s?\w+ |\d{1,},?\d{1,}\s?\w+\/?\w+|\d{1,},?\d{0,}\%)')
+df['DOSAGE'] = df['INFO'].apply(lambda x : findDosage(x,regex_dosage) )
 
-filename = 'medicaments_2.csv'
+# Create FORME column
+df=df.apply(lambda x : getFormePharma(x),axis=1 )
+
+# Delete aux column
+df = df.drop(['INFO'], axis = 1)
+
+
+filename = 'medicaments.csv'
 df.to_csv(filename, encoding='utf-8')
